4adb5d6ee1e98d90ce268836ae6dad9bb6c9ed85
Zoe Kirsh <zoekirsh@gmail.com>
2021-02-08 11:56:26 -0800

    formz

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  validateLogin = e => {
+    e.preventDefault()
+    if (this.state.username.length >= 1 && this.state.password.length >= 1) {
+      this.props.handleLogin(this.state)
+    } else {
+      return (console.log("please type something"))
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.validateLogin}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleInput} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleInput} value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+  
+  handleChange = e => {
+    this.setState({
+      message: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
+        <p>
+          Characters remaining: {this.props.maxChars - this.state.message.length}
+        </p>
       </div>
     );
   }
